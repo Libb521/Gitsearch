fc55f3bace112465d56b5da3623a5356cc74c017
Libb521 <eotieno39@yahoo.com>
2020-01-20 22:00:22 +0300

    removed the 'public' tag as it was not needed

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,7 +8,7 @@ import { ProfileService } from '../profile.service';
 })
 export class ProfileComponent implements OnInit {
 
-  public profile: any;
+  profile: any;
   repos: any;
   username: string;
 
